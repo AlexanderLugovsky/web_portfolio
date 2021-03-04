211756fc31c65092b4313922876da84b0bb1cd85
Alexandr Lugovskiy <73530174+AlexanderLugovsky@users.noreply.github.com>
2021-03-04 10:04:37 +0200

    Прототип

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,21 @@
 function linkMenu() {
 	switch (document.location.pathname) {
 		case "/index.html":
-			document.getElementById("menuHome").innerHTML = '<div class="menu-links-on">Home</div>';
+			document.getElementById("menuHome").innerHTML = '<div class="menu-links-on">Главная</div>';
 			break;
-		case "/work.html":
-			document.getElementById("menuWork").innerHTML = '<div class="menu-links-on">Work</div>';
+		case "/works.html":
+			document.getElementById("menuWorks").innerHTML = '<div class="menu-links-on">Работы</div>';
 			break;
 		case "/logos.html":
 			document.getElementById("menuLogos").innerHTML = '<div class="menu-links-on">Logos</div>';
 			break;
 		case "/about.html":
-			document.getElementById("menuAbout").innerHTML = '<div class="menu-links-on">About</div>';
+			document.getElementById("menuAbout").innerHTML = '<div class="menu-links-on">Обо мне</div>';
 			break;
 		case "/blog.html":
 			document.getElementById("menuBlog").innerHTML = '<div class="menu-links-on">Blog</div>';
 			break;
 		default:
-			document.getElementById("menuHome").innerHTML = '<div id="menuHome" class="menu-links-on">Home</div>';
+			document.getElementById("menuHome").innerHTML = '<div class="menu-links-on">Главная</div>';
 	}
 }
